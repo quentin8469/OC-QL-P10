05684836de640292076a317a8673cc7e6aca1494
Quentin Laparra <quentin.laparra@gmail.com>
2021-08-04 17:04:04 +0200

    Add attr in charfield models

--- API_SoftDesk/AppSoftDesk/admin.py
+++ API_SoftDesk/AppSoftDesk/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Comments, Contributors, Projects, Issues
+from .models import Comments, Contributors, Projects, Issues
 
 # Register your models here.
 
--- API_SoftDesk/AppSoftDesk/models.py
+++ API_SoftDesk/AppSoftDesk/models.py
@@ -13,42 +13,35 @@ class Contributors(models.Model):
     """"""
     user_id = models.IntegerField()
     projet_id = models.IntegerField()
-    permission = models.CharField()
-    role = models.CharField()
-
-    pass
+    permission = models.CharField(max_length=150)
+    role = models.CharField(max_length=150)
 
 
 class Projects(models.Model):
     """"""
-    project_id = models.CharField()
-    title = models.CharField()
-    description = models.CharField()
-    type = models.CharField()
-    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
-    assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    project_id = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    type = models.CharField(max_length=150)
+    author_user_id = models.CharField(max_length=150)
+    assignee_user_id = models.CharField(max_length=150)
     created_time = models.DateTimeField()
-    pass
 
 
 class Issues(models.Model):
     """"""
-    tile = models.CharField()
-    description = models.CharField()
-    tag = models.CharField()
-    priority = models.CharField()
-    projetct_id = models.IntegerField()
-    status = models.CharField()
-
-    pass
+    tile = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    tag = models.CharField(max_length=150)
+    priority = models.CharField(max_length=150)
+    projetct_id = models.CharField(max_length=150) # a modif
+    status = models.CharField(max_length=150)
 
 
 class Comments(models.Model):
     """"""
     comment_id = models.IntegerField()
-    description = models.CharField()
-    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
-    issue_id = models.ForeignKey()
+    description = models.CharField(max_length=150)
+    author_user_id = models.CharField(max_length=150)
+    issue_id = models.CharField(max_length=150) # a modif ForeignKey
     created_time = models.DateTimeField()
-
-    pass
