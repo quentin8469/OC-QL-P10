4e61d32a848690f3362dc5da2ea695d46b2bca3a
Quentin Laparra <quentin.laparra@gmail.com>
2021-08-04 12:36:12 +0200

    Add serializers and work on admin file

--- API_SoftDesk/AppSoftDesk/admin.py
+++ API_SoftDesk/AppSoftDesk/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Comments, Contributors, Projects, Issues
 
 # Register your models here.
+
+admin.site.register(Comments)
+admin.site.register(Contributors)
+admin.site.register(Projects)
+admin.site.register(Issues)
--- API_SoftDesk/AppSoftDesk/serializers.py
+++ API_SoftDesk/AppSoftDesk/serializers.py
@@ -1,1 +1,27 @@
 from rest_framework import serializers
+from models import Comments, Contributors, Projects, Issues
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """"""
+    class Meta:
+        model = Comments
+
+
+class ContributorsSerializer(serializers.ModelSerializer):
+    """"""
+    class Meta:
+        model = Contributors
+
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    """"""
+    class Meta:
+        model = Projects
+
+
+class IssuesSerializer(serializers.ModelSerializer):
+    """"""
+    class Meta:
+        model = Issues
+
