7c927df4952584e22c2301eda5d78f16ca33047d
Quentin Laparra <quentin.laparra@gmail.com>
2021-08-04 21:07:57 +0200

    Add fields in serializers file

--- API_SoftDesk/AppSoftDesk/serializers.py
+++ API_SoftDesk/AppSoftDesk/serializers.py
@@ -6,21 +6,27 @@ class CommentsSerializer(serializers.ModelSerializer):
     """"""
     class Meta:
         model = Comments
+        fields = ['id', 'description', 'author',
+                  'created_time']
 
 
 class ContributorsSerializer(serializers.ModelSerializer):
     """"""
     class Meta:
         model = Contributors
+        fields = ['id', 'user', 'project', 'permission', 'role']
 
 
 class ProjectsSerializer(serializers.ModelSerializer):
     """"""
     class Meta:
         model = Projects
+        fields = ['id', 'title', 'description', 'type']
 
 
 class IssuesSerializer(serializers.ModelSerializer):
     """"""
     class Meta:
         model = Issues
+        fields = ['id', 'title', 'desc', 'tag', 'priority',
+                  'status', 'author', 'assignee', 'created_time']
