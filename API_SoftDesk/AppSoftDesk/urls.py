9e6d1236506a0293d852f6c93936077c8dd51cd2
Quentin Laparra <quentin.laparra@gmail.com>
2021-08-06 04:15:15 +0200

    Add routers

--- API_SoftDesk/AppSoftDesk/urls.py
+++ API_SoftDesk/AppSoftDesk/urls.py
@@ -3,11 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from rest_framework import routers
 
-from .views import ProjectsViewSet
+from .views import ProjectsViewSet, UserViewSet, CommentsViewSet, ContributorsViewSet, IssuesViewSet
 
 router = routers.DefaultRouter()
-router.register('projects', ProjectsViewSet)
+router.register(r'signup', UserViewSet)
+router.register(r'projects', ProjectsViewSet)
+router.register(r'comments', CommentsViewSet)
+router.register(r'contributors', ContributorsViewSet)
+router.register(r'issues', IssuesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
